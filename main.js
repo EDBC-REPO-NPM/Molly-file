00425b3bfda10d57aa52f4a8ec18d424bc626fde
BCED <BecerraCEnmanuelD@gmail.com>
2022-07-22 02:05:04 -0400

    done for npm deploy

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const modules = new Object();
 
-try{ modules.createStreamDB = require('./streamDB'); } catch(e) { /*console.log(e)*/ }
-try{ modules.createMongoDB = require('./mongoDB'); } catch(e) { /*console.log(e)*/ }
-try{ modules.createLocalDB = require('./localDB'); } catch(e) { /*console.log(e)*/ }
-try{ modules.createWebDB = require('./webDB'); } catch(e) { /*console.log(e)*/ }
+try{ modules.createStreamDB = require('./module/streamDB'); } catch(e) { /*console.log(e)*/ }
+try{ modules.createMongoDB = require('./module/mongoDB'); } catch(e) { /*console.log(e)*/ }
+try{ modules.createLocalDB = require('./module/localDB'); } catch(e) { /*console.log(e)*/ }
+try{ modules.createWebDB = require('./module/webDB'); } catch(e) { /*console.log(e)*/ }
 
 module.exports = modules;
