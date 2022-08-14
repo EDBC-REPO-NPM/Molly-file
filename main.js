ee2e3516507c6d5c17b3a11863b37f9df64af609
becerraenmanuel <BecerraCEnmanuelD@gmail.com>
2022-08-13 20:46:06 -0400

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const output = new Object();
 
-try{ output.streamDB = require('./module/streamDB2'); } catch(e) { /*console.log(e)*/ }
+try{ output.streamDB = require('./module/streamDB'); } catch(e) { /*console.log(e)*/ }
 try{ output.localDB = require('./module/localDB'); } catch(e) { /*console.log(e)*/ }
 
 module.exports = output;
